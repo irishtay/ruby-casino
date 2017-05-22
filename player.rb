4ca41831e9bc7a79568320c12f959cad567ac2ee
Zachary Sanders <zsanders@icloud.com>
2017-05-22 12:11:01 -0600

    update player class

--- player.rb
+++ player.rb
@@ -3,12 +3,30 @@ require_relative 'wallet'
 
 class Player
     
-    attr_accessor :wallet, :name
+    attr_accessor :wallet, :name, :age
 
-    def initialize (args)
-       @wallet = args[:wallet] 
-       @name = args[:name]
+    def initialize (wallet)
+       @wallet = wallet 
+       get_player_info
     end
 
+    def get_player_info
+        puts 'What is your name?'
+        name = gets.strip
+        puts 'How old are you?'
+        age = gets.strip
+        puts 'How much money do you have?'
+        wallet.add_money = gets.strip.to_i
+    end
+
+    def check_bet(bet)
+        if bet > wallet.get_balance
+            return false
+        elsif wallet.get_balance == 0
+            #create callateral
+        else
+            return true
+        end
+    end
 
 end
