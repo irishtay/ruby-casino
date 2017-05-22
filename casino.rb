e21f081ebf048bdfa2792151bf964fab420b7e07
Cameron Bullock <bullockcameron@gmail.com>
2017-05-22 12:32:30 -0600

    Casino Code

--- casino.rb
+++ casino.rb
@@ -4,13 +4,33 @@ require 'colorize'
 require_relative 'player'
 
 class Casino
-  attr_accessor :player
+  attr_accessor :people, :games
 
-    def initialize
-        puts "Welcome to our Ruby Casino!"
-        @player = Player.new ( { wallet: Wallet.new, name: "John" } )
-        casino_menu
+  def initialize
+    puts 'Welcome to our Casino'
+    # @people = people.new
+    # @games = games.new
+    display_menu
+  end
+
+  def display_menu
+    puts '1) Begin a Game'
+    puts '2) Order Food and Drink'
+    puts '3) Visit Gift Shop'
+    puts '4) Exit Casino'
+
+    menu_selection = gets.to_i
+
+    case menu_selection
+      when 1
+        # Create Game
+      when 2
+        # Buy Food or Drink
+      when 3 || 4
+        # Visit Gift Shop
     end
+
+  end
 
 
     def casino_menu
@@ -18,5 +38,4 @@ class Casino
     end
 end
 
-
-Casino.new
+casino = Casino.new
