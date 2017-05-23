079682006c7f716cbd9e26db3e94c688099c941d
Cameron Bullock <bullockcameron@gmail.com>
2017-05-23 13:59:07 -0600

    Completed Slots

--- giftshop.rb
+++ giftshop.rb
@@ -16,13 +16,13 @@ class Giftshop
     end
 
     def display_menu
-        
+
         prompt = TTY::Prompt.new
         puts `clear`
         puts "Welcome to the casino gift shop."
         puts "Please take your time and select one of our fine souvenirs. Your current balance is #{player.wallet.get_balance}"
         menu_selection = prompt.select("What would you like to buy?", [(items.map { |item| item.name }), "Go back to the Casino"])
-        
+
         case menu_selection
             when 'I Survived Russian Rouleete T-Shirt'
                 purchased_item = items[0].clone
@@ -39,7 +39,7 @@ class Giftshop
                 puts "Thank you for your purchase. Your balance is now #{player.wallet.get_balance}"
                 exit_casino
             when 'I had to buy this stupid key chain to leave'
-                purchased_item = items[3].clone
+                purchased_item = items[2].clone
                 player.wallet.remove_balance(purchased_item.cost)
                 player.purchased_items << purchased_item
                 puts `clear`
@@ -48,12 +48,12 @@ class Giftshop
             when "Go back to the Casino"
                 casino.display_menu
         end
-   
+
     end
 
     def exit_casino
 
-        
+
         new_prompt = TTY::Prompt.new
         answer = new_prompt.select("What would you like to do?", ["Buy another item", "Go back to the Casino", "Exit Casino"])
         binding.pry
@@ -66,7 +66,7 @@ class Giftshop
                 puts `clear`
                 puts "Goodbye, Please come agian. Next time bring more money"
                 exit
-        end 
+        end
     end
 
 
--- slots.rb
+++ slots.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'tty-prompt'
 
 class Slots
 
@@ -8,39 +9,59 @@ class Slots
     @name = 'Slots'
     @options = gaming_options
     @player = player
-    @reel = ['apple', 'orange', 'cherry', 'bell', 'grapes', 'clover', 'heart', 'diamond', 'bar', 'coin', 'lemon']
+    @reel = ['apple', 'orange', '--', 'cherry', 'bell', '--', 'grapes', 'clover', '--', 'heart', 'diamond', 'bar', 'coin', 'lemon']
+    @reel1 = ""
+    @reel2 = ""
+    @reel3 = ""
+    @play_result = ""
   end
 
   def start_game
-    puts 'Welcome To Slots!'
+    puts `clear`
+    puts "Welcome To Slots!"
     puts "Your Current Balance is #{player.wallet.get_balance}"
-    puts "Place Your Bet"
-    bet = gets.strip.to_i
+    puts "$1 to Play \n"
 
-    puts 'Pull the Lever (Press Enter)'
-    gets
+    puts "-------------"
 
-    print '.'
-    sleep(1)
-    print '.'
-    sleep(1)
-    print ". \n\n"
-    sleep(1)
+    prompt = TTY::Prompt.new
+    play_option = prompt.select("Make a Selection", ["Pull Lever", "Select New Game"])
 
-    puts "#{@reel.sample.capitalize} || #{@reel.sample.capitalize} || #{@reel.sample.capitalize} \n\n"
-    slots_menu
+    if play_option == "Select New Game"
+      options.display_menu_of_games
+    end
+
+    player.wallet.remove_balance(1)
+
+
+    @reel1 = @reel.sample.capitalize
+    @reel2 = @reel.sample.capitalize
+    @reel3 = @reel.sample.capitalize
+
+        sleep(1)
+        print "\n#{@reel1}  "
+        sleep(1)
+        print "#{@reel2}  "
+        sleep(1)
+        print "#{@reel3} \n\n"
+
+    @play_result = "#{@reel1} || #{@reel2} || #{@reel3}"
+
+    results
+
+    puts "Your New Balance is #{player.wallet.get_balance}"
+
   end
 
   def slots_menu
-    puts '1) Play Agian'
-    puts '2) Exit'
+    prompt = TTY::Prompt.new
+    slots_menu_answer = prompt.select("What's Next?", ["Play Again", "Exit"])
 
-    slots_menu_answer = gets.strip.to_i
 
     case slots_menu_answer
-      when 1
+    when "Play Again"
         start_game
-      when 2
+      when "Exit"
         options.display_menu_of_games
       else
         puts "Invalid input, try again"
@@ -49,6 +70,49 @@ class Slots
   end
 
   def results
-    
+    reel_array = [@reel1, @reel2, @reel3]
+    if @play_result == "Bar || Bar || Bar"
+      puts "JACKPOT: 1 Million Dollars!"
+      puts "------"
+      player.wallet.add_money(1000000)
+      puts "Your Balance is now #{player.wallet.get_balance}\n\n"
+
+    elsif reel_array[0] == reel_array[1] && reel_array[1] == reel_array[2] && reel_array != ["bar", "bar", "bar"]
+      puts "3 of a Kind: $100"
+      puts "------"
+      player.wallet.add_money(100)
+      puts "Your Balance is now #{player.wallet.get_balance}\n\n"
+
+    elsif
+      reel_array.count("Apple") == 2 ||
+      reel_array.count("Diamond") == 2 ||
+      reel_array.count("Orange") == 2 ||
+      reel_array.count("Cherry") == 2 ||
+      reel_array.count("Bell") == 2 ||
+      reel_array.count("Grapes") == 2 ||
+      reel_array.count("Clover") == 2 ||
+      reel_array.count("Heart") == 2 ||
+      reel_array.count("Bar") == 2 ||
+      reel_array.count("Coin") == 2 ||
+      reel_array.count("Lemmon") == 2
+
+      puts "2 of a Kind: $10"
+      puts "------"
+      player.wallet.add_money(10)
+      puts "Your Balance is now #{player.wallet.get_balance}\n\n"
+
+
+    elsif reel_array.include?("Diamond")
+      puts "You Got Diamonds: $1"
+      puts "------"
+      player.wallet.add_money(1)
+      puts "Your Balance is now #{player.wallet.get_balance}\n\n"
+
+    else
+      puts "Better Luck Next Time \n\n"
+    end
+  slots_menu
+
   end
+
 end
